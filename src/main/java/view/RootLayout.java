af7f2b08dab805f50c8bed9c84e36c978d28f4d1
Yago Rezende <yagorezende@id.uff.br>
2020-02-09 12:50:04 -0300

    Drag events implemented, icons can now detect drag events

--- src/main/java/view/RootLayout.java
+++ src/main/java/view/RootLayout.java
@@ -1,9 +1,15 @@
 package view;
 
 import controller.DragIcon;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.control.SplitPane;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import model.DragIconType;
@@ -18,6 +24,10 @@ public class RootLayout extends AnchorPane {
     AnchorPane right_pane;
     @FXML
     VBox left_pane;
+
+    private EventHandler mIconDragOverRoot=null;
+    private EventHandler mIconDragDropped=null;
+    private EventHandler mIconDragOverRightPane=null;
 
     private DragIcon mDragOverIcon = null;
 
@@ -43,18 +53,107 @@ public class RootLayout extends AnchorPane {
         //This is added as a child to the root AnchorPane so it can be
         //visible on both sides of the split pane.
         mDragOverIcon = new DragIcon();
-
         mDragOverIcon.setVisible(false);
         mDragOverIcon.setOpacity(0.65);
         getChildren().add(mDragOverIcon);
 
         //populate left pane with multiple colored icons for testing
         for (int i = 0; i < 7; i++) {
-
             DragIcon icn = new DragIcon();
-
+            addDragDetection(icn);
             icn.setType(DragIconType.values()[i]);
             left_pane.getChildren().add(icn);
         }
+        buildDragHandlers();
+    }
+
+    private void buildDragHandlers() {
+        //drag over transition to move widget form left pane to right pane
+        mIconDragOverRoot = new EventHandler <DragEvent>() {
+            /**
+             * whenever it’s triggered, it tests the mouse cursor’s coordinates against
+             * the bounds of the right-hand AnchorPane.  If the cursor is within the bounds,
+             * mDragOverIcon is relocated to follow the cursor.
+             * */
+            @Override
+            public void handle(DragEvent event) {
+                Point2D p = right_pane.sceneToLocal(event.getSceneX(), event.getSceneY());
+                if (!right_pane.boundsInLocalProperty().get().contains(p)) {
+                    mDragOverIcon.relocateToPoint(new Point2D(event.getSceneX(), event.getSceneY()));
+                    return;
+                }
+                event.consume();
+            }
+        };
+
+        mIconDragOverRightPane = new EventHandler <DragEvent> () {
+            @Override
+            public void handle(DragEvent event) {
+                event.acceptTransferModes(TransferMode.ANY);
+                mDragOverIcon.relocateToPoint(new Point2D(event.getSceneX(), event.getSceneY()));
+                event.consume();
+            }
+        };
+
+        mIconDragDropped = new EventHandler <DragEvent> () {
+
+            /**
+             * This DragDropped event handler indicates that the drag event completed successfully,
+             * hides the drag over icon, and removes the event handlers that were installed previously.
+             * */
+
+            @Override
+            public void handle(DragEvent event) {
+                event.setDropCompleted(true);
+                right_pane.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRightPane);
+                right_pane.removeEventHandler(DragEvent.DRAG_DROPPED, mIconDragDropped);
+                base_pane.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRoot);
+                mDragOverIcon.setVisible(false);
+                event.consume();
+            }
+        };
+
+        this.setOnDragDone (new EventHandler <DragEvent> (){
+            @Override
+            public void handle (DragEvent event) {
+                right_pane.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRightPane);
+                right_pane.removeEventHandler(DragEvent.DRAG_DROPPED, mIconDragDropped);
+                base_pane.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRoot);
+                mDragOverIcon.setVisible(false);
+                event.consume();
+            }
+        });
+    }
+
+    private void addDragDetection(DragIcon dragIcon) {
+
+        dragIcon.setOnDragDetected (new EventHandler <MouseEvent> () {
+
+            @Override
+            public void handle(MouseEvent event) {
+
+                // set the other drag event handles on their respective objects
+                base_pane.setOnDragOver(mIconDragOverRoot);
+                right_pane.setOnDragOver(mIconDragOverRightPane);
+                right_pane.setOnDragDropped(mIconDragDropped);
+
+                // get a reference to the clicked DragIcon object
+                DragIcon icn = (DragIcon) event.getSource();
+
+                //begin drag ops
+                mDragOverIcon.setType(icn.getType());
+                mDragOverIcon.relocateToPoint(new Point2D(event.getSceneX(), event.getSceneY()));
+
+                ClipboardContent content = new ClipboardContent();
+                content.putString(icn.getType().toString());
+
+                mDragOverIcon.startDragAndDrop (TransferMode.ANY).setContent(content);
+                mDragOverIcon.setVisible(true);
+                mDragOverIcon.setMouseTransparent(true);
+                event.consume();
+            }
+        });
+
+
     }
 }
